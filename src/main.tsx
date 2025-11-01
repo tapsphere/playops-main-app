3d367d32c7ef55cd09118974ca0db4377f5086bc
SANKARAMOORTHY-DEVELOPER <78921146+SANKARAMOORTHY-DEVELOPER@users.noreply.github.com>
2025-11-01 14:55:31 +0530

    kj

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { TonConnectProvider } from "./providers/TonConnectProvider";
-import { ThirdwebProviderWrapper } from "./providers/ThirdwebProvider";
+import { TonConnectProvider } from "@providers/TonConnectProvider";
+import { ThirdwebProviderWrapper } from "@providers/ThirdwebProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
